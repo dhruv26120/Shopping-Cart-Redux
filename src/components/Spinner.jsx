4f890a8bdf532d3254f74f1b4d15a9a58c5abbe7
dhruv26120 <dhruvkumar110@gmail.com>
2024-01-27 00:50:01 +0530

    Tailwind modified

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import './Spinner.css'
 
 const Spinner = () => {
   return (
-    <div>
+    <div className='w-screen h-[calc(100vh-80px)] flex justify-center items-center'>
       <div className='spinner'></div>
     </div>
   )
